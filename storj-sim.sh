c83f7db8a331a8f100141fb0e9e5cc888f51bb03
Antonio Franco <antonio@storj.io>
2022-11-17 10:44:52 -0500

    feat(storj-sim): storj-sim automation
    Checks to see if cockroach is installed and installs if needed. Configures the database and connection and runs storj-sim.

---
+++ storj-sim.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -ue
+set -o pipefail
+
+trap traperr err
+
+traperr() {
+	echo "ERROR: ${BASH_SOURCE[1]} near line ${BASH_LINENO[0]} while working with ${CurrentRepo}."
+}
+
+if which cockroach >/dev/null; then
+	echo "starting cockroach..."
+	cockroach start-single-node --insecure --listen-addr=localhost&
+	serv_pid[cockroach]="${!}"
+else
+    echo "cockroach not installed or isn't in path. Attempting scripted install."
+	curl https://binaries.cockroachdb.com/cockroach-v21.1.7.linux-amd64.tgz | tar -xz && sudo cp -i cockroach-v21.1.7.linux-amd64/cockroach /usr/local/bin/
+	echo "-----------------------------------------------------------------------------"
+	rm -rf "cockroach-v21.1.7.linux-amd64/cockroach/"
+
+	if which cockroach >/dev/null; then
+		echo "Cockroach installed successfully, starting single-node instance"
+		cockroach start-single-node --insecure --listen-addr=localhost&
+		serv_pid[cockroach]="${!}"
+	fi
+fi
+
+if which storj-sim >/dev/null; then
+	echo "starting storj-sim"
+	storj-sim network destroy
+	storj-sim network setup --postgres=cockroach://root@localhost:26257?sslmode=disable
+	storj-sim network run --no-gateways&
+	serv_pid[storj-sim]="${!}"
+else
+    echo "storj-sim not installed or isn't in path. Try running clone-repos.sh."
+fi
